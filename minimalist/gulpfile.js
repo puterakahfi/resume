134b912ebf198d99eac89bb508a81b9bbfe23699
puterakahfi <puterakahfi@gmail.com>
2016-09-15 23:23:59 +0700

    added some tasks

--- minimalist/gulpfile.js
+++ minimalist/gulpfile.js
@@ -1,6 +1,40 @@
-gulp = require('gulp');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
-gulp.task('default', function() {
-      // place code for your default task here
-    //
+gulp.task('default', ['browser-sync', 'watch']);
+
+
+gulp.task('watch',function(done){
+    gulp.watch(['src/**/*.css'], ['copy-css']);
+});
+
+// static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// copy fonts to dist
+gulp.task('copy-fonts', function(){
+    return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+});
+
+// copy css files
+gulp.task('copy-css',function(){
+    return gulp.src('src/css/**/*.css').pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+// copy js files
+gulp.task('copy-js',function(){
+    return gulp.src('src/js/**/*.js').pipe(gulp.dest('dist/js'));
+});
+
+//copy third party package
+gulp.task('copy-third',function(){
+    return gulp.src('src/packages/**/*').pipe(gulp.dest('dist/packages'));
 });
