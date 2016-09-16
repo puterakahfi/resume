665e3e3dcc3c12784cea02d8393a9d48b96f9b78
puterakahfi <puterakahfi@gmail.com>
2016-09-16 16:51:40 +0700

    added build task in default task

--- minimalist/gulpfile.js
+++ minimalist/gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var cleanCSS = require('gulp-clean-css');
 var reload = browserSync.reload;
-gulp.task('default', ['browser-sync', 'watch']);
+
+
+gulp.task('default', ['build','browser-sync', 'watch']);
 
 
 gulp.task('watch',function(done){
