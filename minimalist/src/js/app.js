f84fd9166f60c9e9eca86574ffa49898018113e9
puterakahfi <puterakahfi@gmail.com>
2016-09-22 21:44:49 +0700

    updade profile component, access json data

--- minimalist/src/js/app.js
+++ minimalist/src/js/app.js
@@ -1,9 +1,20 @@
+var profile = Vue.http.get('resume.json')
+    .then((response) =>{ 
+        return  response.json(); 
+    })
+    .catch( function(error) { 
+        console.error(error); 
+    });
+console.log(profile);
+
 Vue.component('profile',{
-    template: '#profile'
+    template: '#profile',
+    data : function(){
+        return profile
+    }
 });
 
-
 new Vue({
-    el: '#app'
+    el: '#app',
 })
 
