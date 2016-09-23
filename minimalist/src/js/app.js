8e080f7334997817458cabf71be19440c07ea66f
puterakahfi <puterakahfi@gmail.com>
2016-09-23 21:22:23 +0700

    load data from resume.json

--- minimalist/src/js/app.js
+++ minimalist/src/js/app.js
@@ -2,22 +2,20 @@ Vue.component('profile',{
     template: '#profile',
     data : function(){
         return {
-            profile: fetchProfile() 
+            profile: [] 
         }},
-    ready: function(){
+    mounted: function(){
         this.fetchProfile();
     },
     methods: {
         fetchProfile: function(){
-            alert("call");
-            var profile= [];
-            var profile = Vue.http.get('resume.json')
-            .then((response) =>{ 
-                alert(response.json());
-                return  response.json(); 
+             Vue.http.get('resume.json')
+            .then((response) =>{
+                console.log(response.body);
+                this.profile = response.body.basics;
             })
-                .catch( function(error) { 
-                    console.error(error); 
+                .catch( function(error) {
+                    console.error(error);
                 });
         }
     }
