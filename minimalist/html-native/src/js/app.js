d323a010ab1af691147191566efd6e70aa92b8b6
puterakahfi <puterakahfi@gmail.com>
2016-09-30 13:28:03 +0700

    init html native version

---
+++ minimalist/html-native/src/js/app.js
@@ -0,0 +1,28 @@
+Vue.component('profile',{
+    template: '#profile',
+    data : function(){
+        return {
+            profile:[]
+        }},
+    ready: function(){
+        this.fetchProfile();
+    },
+    methods: {
+        fetchProfile: function(){
+            var profile= [];
+            var profile = Vue.http.get('resume.json')
+            .then((response) =>{ 
+                alert(response.json());
+                return  response.json(); 
+            })
+                .catch( function(error) { 
+                    console.error(error); 
+                });
+        }
+    }
+});
+
+new Vue({
+    el: '#app',
+})
+
